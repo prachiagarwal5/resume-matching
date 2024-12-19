ab6248811078195a6ab7b4dc3363f448b833ae7e
anoopkumargithub <anoopkumar7818@outlook.com>
2024-12-19 18:13:53 +0530

    add muiltple files

--- backend/routes/analyzeRoutes.js
+++ backend/routes/analyzeRoutes.js
@@ -1,12 +1,3 @@
-// const express = require('express');
-// const { analyzeResumeController } = require('../controllers/analyzeController');
-// const router = express.Router();
-
-// // POST route for resume analysis
-// router.post('/', analyzeResumeController);
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/upload");
