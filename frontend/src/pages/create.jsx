e4e3d9d796c3a37e4c165f9a412aac812ea5727e
Anoop Kumar <113820345+Anoopkumargithub@users.noreply.github.com>
2025-03-25 09:30:37 +0530

    Update create.jsx backend api rou

--- frontend/src/pages/create.jsx
+++ frontend/src/pages/create.jsx
@@ -1138,7 +1138,7 @@ const CreateResume = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5001/api/form/submit",
+        "https://resumeanalyser-nhai.onrender.com/api/form/submit",
         formData,
       );
       console.log("Data sent successfully:", response.data);
