f82d35a804b01fe10267fb8012acb0336172e2dc
Anoop Kumar <113820345+Anoopkumargithub@users.noreply.github.com>
2025-03-27 01:07:03 +0530

    Update Template3.jsx

--- frontend/src/Component/templates/Template3.jsx
+++ frontend/src/Component/templates/Template3.jsx
@@ -174,6 +174,7 @@ const Template3 = ({ data }) => {
                   <View style={styles.educationRow}>
                     <View style={styles.educationCol}>
                       <Text style={styles.institutionName}>
+                        {resumeData?.enducation?.graduation?.degree},
                         {resumeData?.education?.graduation?.institution},{" "}
                         {resumeData?.education?.graduation?.location} |
                         {resumeData?.education?.graduation?.CPI}
@@ -193,7 +194,8 @@ const Template3 = ({ data }) => {
                 <View style={styles.educationTable}>
                   <View style={styles.educationRow}>
                     <View style={styles.educationCol}>
-                      <Text style={styles.institutionName}> Intermediate, 
+                      <Text style={styles.institutionName}> 
+                        Intermediate, 
                         {resumeData?.education?.intermediate?.schoolName},{" "}
                         {resumeData?.education?.intermediate?.location} |
                         {resumeData?.education?.intermediate?.percentage}
@@ -213,7 +215,8 @@ const Template3 = ({ data }) => {
                 <View style={styles.educationTable}>
                   <View style={styles.educationRow}>
                     <View style={styles.educationCol}>
-                      <Text style={styles.institutionName}> High School, 
+                      <Text style={styles.institutionName}> 
+                        High School, 
                         {resumeData?.education?.highSchool?.schoolName},{" "}
                         {resumeData?.education?.highSchool?.location} |
                         {resumeData?.education?.highSchool?.percentage}
