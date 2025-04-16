e4933af27e0da833d7cf7ddad4e215765d232240
Anoop Kumar <113820345+Anoopkumargithub@users.noreply.github.com>
2025-04-16 14:06:10 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 // Configure CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://gla-fit.onrender.com/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
